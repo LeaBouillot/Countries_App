b3492c8ac1538363e5571570fe93e912ad729983
bestdevelope <leabouillot7@gmail.com>
2024-06-15 23:57:56 +0200

    afficher country info list

--- script.js
+++ script.js
@@ -4,13 +4,32 @@ const input = form.querySelector("input");
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
-  if (input.value.trim() != "") {
+  if (input.value.trim() != " ") {
     showCountries(input.value);
   } else {
     alert("Merci de rechercher le pay");
   }
 });
 
+const showCountries = async (search) => {
+  const request = await fetch(`https://restcountries.com/v3.1/name/${search}`);
+  console.log(request);
+  const countries = await request.json();
+  console.log(countries);
+
+  let list = "";
+
+  countries.forEach((country) => {
+    list += `
+           <div>
+        <h2>${country.traslations.fra.common}</h2>
+        <img src="${country.flags.svg}" alt="${country.flags.alt}">
+        <h3>${country.contients[0]}</h3>
+        <h4></h4>
+         </div> `;
+
+         grid.innerHTML = list;
+  })
 
 
-
+};
